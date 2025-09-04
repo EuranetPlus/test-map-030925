04046cba5bb31d57bc3cc583df8be5be8ebda4bc
Euranet Plus <104374044+EuranetPlus@users.noreply.github.com>
2025-09-04 12:47:56 +0200

    Update config-features.js

--- src/lib/stores/config-features.js
+++ src/lib/stores/config-features.js
@@ -1,5 +1,5 @@
 export const config = {
-	mapTitle: 'employment-tertiary-attainment', // The title of the map, if more than one word, separate by dash, e.g. "income-europe"
+	mapTitle: 'test-map-030925', // The title of the map, if more than one word, separate by dash, e.g. "income-europe"
 	vercelURL: 'map-employment-tertiary-attainment.vercel.app', // The url provided when deploying the map on Vercel
 	datasetType: 'values', // "values" or "binary"; Does the data set contain numerical values or binary (0/1) values for countries?
 	datasetUnit: 'percent', // "fullNumbers" or "percent"; Is the data in percent (0.25 of GDP) or full numbers (25 people)?
